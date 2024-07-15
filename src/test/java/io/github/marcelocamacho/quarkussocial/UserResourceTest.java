0a1fb3d1e145ff569abc32034b3e4117cf8cd3d8
Marcelo Camacho <marcelocamacho.ufpa@gmail.com>
2024-07-15 08:26:55 -0300

    Cria os testes para o método GET /USERS

--- src/test/java/io/github/marcelocamacho/quarkussocial/UserResourceTest.java
+++ src/test/java/io/github/marcelocamacho/quarkussocial/UserResourceTest.java
@@ -5,22 +5,32 @@ import static io.restassured.RestAssured.useRelaxedHTTPSValidation;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+import java.net.URL;
 import java.util.List;
 import java.util.Map;
 
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 
 import io.github.marcelocamacho.quarkussocial.dto.CreateUserRequest;
 import io.github.marcelocamacho.quarkussocial.dto.ResponseError;
+import io.quarkus.test.common.http.TestHTTPResource;
 import io.quarkus.test.junit.QuarkusTest;
 import io.restassured.http.ContentType;
 
 @QuarkusTest
+//@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class UserResourceTest {
+
+    @TestHTTPResource("/users")
+    URL apiURL;
     
     @Test
     @DisplayName("Should create an user successfully")
+    //@Order(1)
     public void createUserTest(){
         var user = new CreateUserRequest();
         user.setName("Fulano");
@@ -53,4 +63,27 @@ public class UserResourceTest {
             assertNotNull(errors.get(1).get("message"));
             assertEquals("Age field is required.", errors.get(0).get("message"));
     }
+
+    @Test
+    @DisplayName("Should list all users")
+    //@Order(2)
+    public void listAllUsersTest(){
+        given().contentType(ContentType.JSON)
+            .when().get(apiURL)
+            .then().statusCode(200)
+            .body("size()",Matchers.is(0));
+        
+            var user = new CreateUserRequest();
+            user.setName("Fulano");
+            user.setAge(22);
+            given().contentType(ContentType.JSON).body(user)
+            .when().post("/users");
+
+            given().contentType(ContentType.JSON)
+            .when().get(apiURL)
+            .then().statusCode(200)
+            .body("size()",Matchers.is(1));
+    }
+
 }
+
